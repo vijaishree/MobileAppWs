9656430a5d7f8982bb4da831d456de234c193b1c
Vijai Shree <vijaishreeseenivasagan@gmail.com>
2024-04-24 18:56:45 +0530

    creates a new getUser method that requests parameters page limit and sort

--- src/main/java/com/appdeveloperblog/app/ws/ui.controller/UserController.java
+++ src/main/java/com/appdeveloperblog/app/ws/ui.controller/UserController.java
@@ -3,9 +3,16 @@ package com.appdeveloperblog.app.ws.ui.controller;
 
 import org.springframework.web.bind.annotation.*;
 
+
+
 @RestController
 @RequestMapping("users")  //https://localhost:8080/users
 public class UserController {
+
+    @GetMapping
+    public String getUser(@RequestParam(value = "page" ,defaultValue="1") int page  ,@RequestParam(value="limit",defaultValue = "50")int limit, @RequestParam(value = "sort", required = false)String sort ){
+        return "no.of pages = "+ page + "limit ="+limit+"sort = "+sort ;//http://localhost:8080/users?page=1&limit=50
+    }
 
     @GetMapping(path="/{userId}")
     public String getUser(@PathVariable String userId){
